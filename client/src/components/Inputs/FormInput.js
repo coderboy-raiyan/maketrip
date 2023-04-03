60d8fe63afed270f53ad275fbca65c0f14bbf597
Raiyan <tajkierhaque@gmail.com>
2023-04-03 13:18:28 +0600

    form input design has been updated [client/FormInput #4]

--- client/src/components/Inputs/FormInput.js
+++ client/src/components/Inputs/FormInput.js
@@ -1,7 +1,7 @@
 function FormInput({ onBlur, onChange, type, value, placeholder, name, styles }) {
     return (
         <input
-            className={`rounded-lg border-[#EAEAEA] py-3 text-[15px] placeholder:text-[14px] placeholder:text-[#B8B9BF] ${styles}`}
+            className={`rounded-lg border-[#EAEAEA] py-3 text-[15px] placeholder:text-[14px] placeholder:text-[#B8B9BF] focus:outline-none focus:ring-0 ${styles}`}
             onChange={onChange}
             onBlur={onBlur}
             type={type}
